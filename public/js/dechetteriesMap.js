eb0585169d772d0e5b4bf947e188aa107de05916
guilbill <guilbill@gmail.com>
2014-02-28 17:17:51 +0100

    Passage à leaflet

--- public/js/dechetteriesMap.js
+++ public/js/dechetteriesMap.js
@@ -32,34 +32,93 @@ var geolocate = function(markers, map) {
         var coords = e.position.coords;
         var geolocatePosition = createPosition(coords.longitude, coords.latitude);
         map.setCenter(geolocatePosition, 12);
-        var icon = new OpenLayers.Icon("/img/home-orange.png", size, offset);
+        var icon = new OpenLayers.Icon("/img/home-red.png", size, offset);
         markers.addMarker(new OpenLayers.Marker(geolocatePosition, icon));
     });
     geolocate.activate();
     geolocate.getCurrentLocation();
 }
 
-var drawMarkers = function (inputMarkers, markers) {
-        for (var i = inputMarkers.length - 1; i >= 0; i--) {
-            var dechetteriePosition = inputMarkers[i].position;
-            var latitude = dechetteriePosition.latitude;
-            var longitude = dechetteriePosition.longitude;
-            var currentPosition = createPosition(longitude, latitude);
-            var icon = new OpenLayers.Icon("/img/flag-orange.png", size, offset);
-            markers.addMarker(new OpenLayers.Marker(currentPosition, icon))
-        }
-    }
+var getDechetterieDescription = function(dechetterie){
+    var description = "<p>";
+    description += "<h4>Déchetterie de " + dechetterie.ville +"</h4>";
+    var horaire = dechetterie.horaires[0];
+    description += ("Lundi : " + horaire["lundi"].join("-")+"</br>");
+    description += ("Mardi : " + horaire["mardi"].join("-")+"</br>");
+    description += ("Mercredi : " + horaire["mercredi"].join("-")+"</br>");
+    description += ("Jeudi : " + horaire["jeudi"].join("-")+"</br>");
+    description += ("Vendredi : " + horaire["vendredi"].join("-")+"</br>");
+    description += ("Samedi : " + horaire["samedi"].join("-")+"</br>");
+    description += ("Dimanche : " + horaire["dimanche"].join("-")+"</br>");
+    description += "</p>"
+    return description;
+}
 
+var drawMarkers = function(inputMarkers, map) {
+    var flagIcon = L.icon({
+        iconUrl: '/img/flag-red.png'
+    });
+    for (var i = inputMarkers.length - 1; i >= 0; i--) {
+        var dechetterie = inputMarkers[i];
+        var dechetteriePosition = dechetterie.position;
+        var latitude = dechetteriePosition.latitude;
+        var longitude = dechetteriePosition.longitude;
+        var markerConf = {
+            icon: flagIcon,
+            title: "Déchetterie de " + dechetterie.ville,
+            riseOnHover: true
+        }
+        var marker = L.marker([latitude, longitude], markerConf).addTo(map);
+        marker.on('click',function(e){
+            var popup = L.popup()
+            .setLatLng(e.latlng) //(assuming e.latlng returns the coordinates of the event)
+            .setContent(getDechetterieDescription(dechetterie))
+            .openOn(map);
+        });
+    }
+}
+
+var drawUsers = function(markers) {
+    $.get("/users", function(data) {
+        for (var i = data.length - 1; i >= 0; i--) {
+            var user = data[i];
+            var userCoords = user.profile.location.split(',');
+            var userIcon = new OpenLayers.Icon("/img/male-user-orange.png", size, offset);
+            var userPosition = createPosition(userCoords[0], userCoords[1]);
+            markers.addMarker(new OpenLayers.Marker(userPosition, userIcon))
+        };
+    });
+}
 var displayMap = function(dechetteries) {
 
-    var map = new OpenLayers.Map("dechetteriesMap");
-    var mapnik = new OpenLayers.Layer.OSM();
-    map.addLayer(mapnik);
-    var position = createPosition(6.1734, 48.6881); //48.6881/6.1734
-    var markers = new OpenLayers.Layer.Markers("Markers");
-    map.addLayer(markers);
-    drawMarkers(dechetteries, markers);
-    geolocate(markers, map);
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {
+        minZoom: 8,
+        maxZoom: 12,
+        attribution: osmAttrib
+    });
+
+    var map = L.map('dechetteriesMap');
+    map.setView(new L.LatLng(48.6881, 6.1734), 12);
+    map.addLayer(osm);
+    // var map = new OpenLayers.Map("dechetteriesMap");
+    // var mapnik = new OpenLayers.Layer.OSM();
+    // map.addLayer(mapnik);
+    // var position = createPosition(6.1734, 48.6881); //48.6881/6.1734
+    // var markers = new OpenLayers.Layer.Markers("Markers");
+    // map.addLayer(markers);
+    drawMarkers(dechetteries, map);
+    // geolocate(markers, map);
+    // $.get("/users", function(data){
+    //     for (var i = data.length - 1; i >= 0; i--) {
+    //         var user = data[i];
+    //         var userCoords = user.profile.location.split(',');
+    //         var userIcon = new OpenLayers.Icon("/img/male-user-orange.png", size, offset);
+    //         var userPosition = createPosition(userCoords[0], userCoords[1]);
+    //         markers.addMarker(new OpenLayers.Marker(userPosition, userIcon))
+    //     };
+    // });
 };
 
 $("#dechetteriesMap").ready(function() {
