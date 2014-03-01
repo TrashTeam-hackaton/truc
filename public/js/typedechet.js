67ba44a27a193253c35ce10c23a32292907cc6be
guilbill <guilbill@gmail.com>
2014-03-01 11:17:16 +0100

    Suppression d'un console.log de debug

--- public/js/typedechet.js
+++ public/js/typedechet.js
@@ -5,7 +5,6 @@ $("#type").ready(function() {
   $('#type').change(function(e){
     var typeDeDechet = e.target.value;
     $.get("/opendata/dechetteries_cugn.json?dechet=" + typeDeDechet, drawMarkers);
-    console.log('on est passés là !');
   });
 
 });
