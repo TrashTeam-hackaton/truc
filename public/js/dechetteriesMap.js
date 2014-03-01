a2ebf470b1ad33ebc4da9f722674c486328d83d6
laryakan <willpaulgm@gmail.com>
2014-03-01 01:11:54 +0100

    Réduction du zoom par défaut

--- public/js/dechetteriesMap.js
+++ public/js/dechetteriesMap.js
@@ -23,7 +23,7 @@ var geolocate = function(map) {
         setView: true
     }).on('locationerror', function() {
         // Si la geolocasition n'a pas marchée
-        map.setView(new L.LatLng(48.6833, 6.2), 12);
+        map.setView(new L.LatLng(48.6833, 6.2), 8);
     });
 }
 
@@ -89,7 +89,7 @@ var displayMap = function(dechetteries) {
     });
 
     var map = L.map('dechetteriesMap');
-    map.setView(new L.LatLng(48.6833, 6.2), 12);
+    map.setView(new L.LatLng(48.6833, 6.2), 8);
     map.addLayer(osm);
     drawMarkers(dechetteries, map);
     geolocate(map);
